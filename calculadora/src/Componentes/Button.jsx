5080ae60c46b92abfbf901664675ec3634fe90d6
Douglas-00 <alves.douglas547@gmail.com>
2021-09-17 15:42:02 -0300

    Projeto feito com React

--- calculadora/src/Componentes/Button.jsx
+++ calculadora/src/Componentes/Button.jsx
@@ -1,5 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
 import './Button.css'
 
-export default props =>
-<button className='button'>{props.label}</button>
+export default props => 
+    <button className={`
+    button
+    ${props.operation ? 'operation':''}
+    ${props.double ? 'double' :''}
+    ${props.triple ? 'triple' :''}
+
+    
+    
+    `}
+    onClick={e => props.click && props.click(e.target.label)}>
+    
+        
+        {props.label}
+        
+    </button>
